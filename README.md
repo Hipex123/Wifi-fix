b6f6fbbcb333e649bcf883ba010afcd81003cc21
Hipex123 <117768734+Hipex123@users.noreply.github.com>
2024-05-15 17:45:35 +0200

    Ip reset

---
+++ README.md
@@ -0,0 +1,12 @@
+PURPOSE
+-------------------------
+
+To reset you IP when when you are having trouble with it.
+
+
+USAGE
+-------------------------
+Put folder in variable path and use command "fix" in cmd.
+You should wait until your user input line shows after you executed command.
+
+NOTE: CMD should be open with administrator privileges.
---
+++ fix.bat
@@ -0,0 +1,15 @@
+@echo off
+REM Set the file containing the commands
+set commandsFile="C:\Users\123\OneDrive\Namizje\Fixes\fix_commands.txt"
+
+REM Check if the commands file exists
+if not exist %commandsFile% (
+    echo Commands file not found: %commandsFile%
+    exit /b 1
+)
+
+REM Read and execute each command from the file
+for /f "usebackq delims=" %%a in (%commandsFile%) do (
+    echo Executing: %%a
+    %%a
+)
